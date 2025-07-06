41ecc26b974b34139545958740c28cfd1a81c498
Ada <the.nostra.tymus@gmail.com>
2025-07-05 22:27:34 -0400

    made processes on the ERD

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,19 @@
-CREATE TABLE todos (
-  id SERIAL PRIMARY KEY,
-  task TEXT NOT NULL,
-  completed BOOLEAN DEFAULT FALSE,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- Create the users table
+CREATE TABLE users (
+    UserID SERIAL PRIMARY KEY,
+    name VARCHAR(25) NOT NULL,
+    hash VARCHAR(64) NOT NULL,
+    salt INTEGER NOT NULL,
+    lastfailedlogin TIMESTAMP,
+    preferences TEXT
 );
 
-INSERT INTO todos (task) VALUES ('Sample task');
 
+CREATE TABLE projects (
+    ProjectID SERIAL PRIMARY KEY,
+    UserID INTEGER NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (UserID) REFERENCES users(UserID) ON DELETE CASCADE
+);
