a3a5df82f251d37eeda0565abe4c64045bdf8221
Ada <the.nostra.tymus@gmail.com>
2025-07-16 14:49:49 -0400

    updated pages endpoint and added logging

--- api/app/logging.py
+++ api/app/logging.py
@@ -26,3 +26,30 @@ def get_project_access_requests():
     cursor.close()
     conn.close()
     return requests
+
+
+def create_page_access_request(session_id, page_id, allowed):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO pagerequests (sessionID, pageID, accessGranted)
+        VALUES (%s, %s, %s);
+    """, (session_id, page_id, allowed))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def get_page_access_requests():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+    SELECT accessTime, accessGranted, ipAddress, name FROM pageRequests
+    JOIN pages on page.PageID = page.PageID
+    JOIN sessions on sessions.sessionID = pageRequests.sessionID
+    """)
+    requests = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return requests
--- api/app/pages.py
+++ api/app/pages.py
@@ -1,8 +1,224 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, make_response
+from http import HTTPStatus as STATUS
+from datetime import datetime
 
-pages_bp = Blueprint('pages', __name__)
+pages_bp = Blueprint('pages', __name__, url_prefix='/pages')
+from .db import get_db_connection
+from .sessions import verify_session_for_access
+from .projects import authorized_project_access
+from .logging import create_page_access_request, get_page_access_requests
+
+page_fields = ['PageID', 'ProjectID', 'name', 'content', 'timeCreated', 'lastEditTime', 'timeInvestment']
 
 
-@pages_bp.route('/test',  methods=['GET'])
-def report():
+@pages_bp.route('/test', methods=['GET'])
+def test_ep():
     return jsonify({"test": "Pages  Endpoint Reached."})
+
+
+def create_page(project_id, name, content):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO pages (ProjectID, name, content, lastEditTime)
+        VALUES (%s, %s, %s, %s)
+        RETURNING *;
+    """, (project_id, name, content, datetime.now()))
+    new_page = cursor.fetchone()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    if new_page is not None:
+        new_page = {k: v for k, v in zip(page_fields, new_page)}
+    return new_page
+
+
+def get_page_by_id(page_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM pages where pageID = %s;", (page_id,))
+    page = cursor.fetchone()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    if page is not None:
+        page = {k: v for k, v in zip(page_fields, page)}
+    return page
+
+
+def update_page(page_id, name):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        UPDATE pages SET name = %s
+        WHERE PageID = %s
+        RETURNING *;
+    """, (name, page_id,))
+    updated_page = cursor.fetchone()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    if updated_page is not None:
+        updated_page = {k: v for k, v in zip(page_fields, updated_page)}
+    return updated_page
+
+
+def delete_page(page_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM pages where PageID = %s;", (page_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def update_content(page_id, content):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        UPDATE pages SET content = %s, lastEditTime = %s
+        WHERE PageID = %s;
+    """, (content, datetime.now(), page_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+@pages_bp.route('/create', methods=['POST'])
+def create_ep():
+    data = request.get_json()
+    project_id = data.get("project_id")
+    name = data.get("name")
+    content = f'Created On: {datetime.now()}'
+
+    token = request.cookies.get("token")
+
+    valid, session = verify_session_for_access(token)
+
+    if not valid:
+        return make_response("Session is Invalid", STATUS.FORBIDDEN)
+
+    if not authorized_project_access(token, project_id):
+        return make_response("Not Authorized To Access Project", STATUS.FORBIDDEN)
+
+    new_page = create_page(project_id, name, content)
+
+    if new_page is None:
+        return make_response("Failed To Create Page", STATUS.INTERNAL_SERVER_ERROR)
+
+    create_page_access_request(session['SessionID'], new_page['PageID'], True)
+
+    response = make_response(f"Created: {name}", STATUS.OK)
+    return response
+
+
+@pages_bp.route('/update', methods=['PATCH'])
+def update_ep():
+    data = request.get_json()
+    page_id = data.get("page_id")
+    name = data.get("name")
+
+    token = request.cookies.get("token")
+
+    valid, session = verify_session_for_access(token)
+
+    if not valid:
+        create_page_access_request(session['SessionID'], page_id, valid)
+        return make_response("Session is Invalid", STATUS.FORBIDDEN)
+
+    page = get_page_by_id(page_id)
+
+    if not authorized_project_access(token, page['ProjectID']):
+        create_page_access_request(session['SessionID'], page_id, False)
+        return make_response("Not Authorized To Access Page", STATUS.FORBIDDEN)
+
+    updated_page = update_page(page_id, name)
+
+    if updated_page is None:
+        return make_response("Failed To Create Page", STATUS.INTERNAL_SERVER_ERROR)
+
+    create_page_access_request(session['SessionID'], updated_page['PageID'], True)
+
+    response = make_response(f"Updated: {name}", STATUS.OK)
+    return response
+
+
+@pages_bp.route('/content', methods=['PUT'])
+def content_ep():
+    data = request.get_json()
+    page_id = data.get("page_id")
+    content = data.get("content")
+
+    token = request.cookies.get("token")
+
+    valid, session = verify_session_for_access(token)
+
+    if not valid:
+        create_page_access_request(session['SessionID'], page_id, valid)
+        return make_response("Session is Invalid", STATUS.FORBIDDEN)
+
+    page = get_page_by_id(page_id)
+
+    if not authorized_project_access(token, page['ProjectID']):
+        create_page_access_request(session['SessionID'], page_id, False)
+        return make_response("Not Authorized To Access Page", STATUS.FORBIDDEN)
+
+    update_content(page_id, content)
+    create_page_access_request(session['SessionID'], page_id, valid)
+
+    response = make_response(f"Updated: {page_id}", STATUS.OK)
+    return response
+
+
+@pages_bp.route('/delete', methods=['DELETE'])
+def delete_ep():
+    data = request.get_json()
+    page_id = data.get("page_id")
+
+    token = request.cookies.get("token")
+
+    valid, session = verify_session_for_access(token)
+
+    if not valid:
+        create_page_access_request(session['SessionID'], page_id, valid)
+        return make_response("Session is Invalid", STATUS.FORBIDDEN)
+
+    page = get_page_by_id(page_id)
+
+    if not authorized_project_access(token, page['ProjectID']):
+        create_page_access_request(session['SessionID'], page_id, False)
+        return make_response("Not Authorized To Access Page", STATUS.FORBIDDEN)
+
+    create_page_access_request(session['SessionID'], page['PageID'], valid)
+    delete_page(page_id)
+
+    response = make_response(f"Deleted: {page}", STATUS.OK)
+    return response
+
+
+@pages_bp.route('/get', methods=['GET'])
+def get_page_ep():
+    page_id = int(request.args.get("id"))
+
+    token = request.cookies.get("token")
+
+    valid, session = verify_session_for_access(token)
+
+    if not valid:
+        create_page_access_request(session['SessionID'], page_id, valid)
+        return make_response("Invalid Session", STATUS.FORBIDDEN)
+
+    page = get_page_by_id(page_id)
+
+    if not authorized_project_access(token, page['ProjectID']):
+        create_page_access_request(session['SessionID'], page_id, False)
+        return make_response("Not Authorized To Access Page", STATUS.FORBIDDEN)
+
+    if page is None:
+        response = make_response("Does Not Exist", STATUS.OK)
+        return response
+
+    create_page_access_request(session['SessionID'], page['PageID'], valid)
+
+    response = make_response(page, STATUS.OK)
+    return response
