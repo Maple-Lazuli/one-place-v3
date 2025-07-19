0be9986e3125805878d5669591d30e59fd5a86de
Ada <the.nostra.tymus@gmail.com>
2025-07-19 02:16:17 -0400

    Started work on the snippets

---
+++ ui/src/components/projects/project/pages/SnippetCard.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import {
+  Card,
+  CardContent,
+  Typography,
+  Box,
+  Button,
+  Stack
+} from '@mui/material'
+import { Link, useParams } from 'react-router-dom'
+
+export default function SnippetCard ({
+  name,
+  language,
+  description,
+  code_id,
+  onDelete
+}) {
+  const { project_id, page_id } = useParams()
+
+  const handleDelete = async code_id => {
+    try {
+      const res = await fetch('/api/code_snippets/delete', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include', // to send the cookie
+        body: JSON.stringify({ snippet_id: code_id })
+      })
+
+      const data = await res.json()
+      if (data.status === 'success') {
+        onDelete?.(code_id) // Notify parent to remove the card
+      } else {
+        console.error(data.message)
+      }
+    } catch (err) {
+      console.error('Delete failed', err)
+    }
+  }
+
+  return (
+    <Card
+      sx={{
+        maxWidth: 400,
+        mb: 2,
+        opacity: isPast ? 0.5 : 1
+      }}
+    >
+      <CardContent>
+        <Typography variant='h6' gutterBottom>
+          {name}
+        </Typography>
+        <Typography variant='subtitle2' color='text.secondary' gutterBottom>
+          {language}
+        </Typography>
+        <Box sx={{ whiteSpace: 'pre-line', mb: 2 }}>
+          <Typography variant='body2' color='text.primary'>
+            {description}
+          </Typography>
+        </Box>
+
+        <Stack direction='row' spacing={1}>
+          <Button
+            variant='outlined'
+            component={Link}
+            to={`/projects/project/${project_id}/pages/page/${page_id}/snippets/view/${code_id}`}
+          >
+            View
+          </Button>
+          <Button
+            variant='outlined'
+            component={Link}
+            to={`/projects/project/${project_id}/pages/page/${page_id}/snippets/update/${code_id}`}
+          >
+            Edit
+          </Button>
+          <Button
+            variant='outlined'
+            color='error'
+            onClick={() => handleDelete(code_id)}
+          >
+            Delete
+          </Button>
+        </Stack>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ ui/src/components/projects/project/pages/Snippets.jsx
@@ -0,0 +1,72 @@
+import { useParams, Link } from 'react-router-dom'
+import {
+  Typography,
+  Box,
+  Button,
+  Grid
+} from '@mui/material'
+import { useEffect, useState } from 'react'
+import SnippetCard from './SnippetCard'
+
+export default function Snippets() {
+  const {project_id,  page_id } = useParams()
+  const [snippets, setSnippets] = useState([])
+
+  useEffect(() => {
+    async function fetchSnippets() {
+      const res = await fetch(`/api/code_snippet/get_all_by_page?id=${page_id}`, {
+        credentials: 'include',
+      })
+      const data = await res.json()
+      if (data.status === 'success') {
+        setSnippets(data.message)
+      }
+    }
+
+    fetchSnippets()
+  }, [page_id])
+
+  const handleDelete = (deletedId) => {
+    setSnippets((prev) => prev.filter((snippet) => snippet.CodeID !== deletedId))
+  }
+
+
+  const sortedSnippets = [...snippets]
+    .sort((a, b) => a.lastEditTime - b.lastEditTime)
+
+  return (
+    <Box sx={{ p: 2 }}>
+      <Typography variant="h5" gutterBottom>
+        Snippets for Page {page_id}
+      </Typography>
+
+      <Typography variant="body1" sx={{ mb: 2 }}>
+        Snippet Stuff
+      </Typography>
+
+      <Button
+        component={Link}
+        to={`/projects/project/${project_id}/pages/page/${page_id}/snippets/create`}
+        variant="contained"
+        sx={{ mb: 3 }}
+      >
+        Create New Snippet
+      </Button>
+
+      <Typography variant="h6" gutterBottom>Snippets</Typography>
+      <Grid container spacing={2}>
+        {sortedSnippets.map((snippet) => (
+          <Grid key={snippet.CodeID}>
+            <SnippetCard
+              name={snippet.name}
+              language={snippet.language}
+              description={snippet.description}
+              code_id={snippet.CodeID}
+              onDelete={handleDelete}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  )
+}
